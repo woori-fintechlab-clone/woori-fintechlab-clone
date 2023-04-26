57451c27fc83f43edadaceeca6216cd854f6d05e
박지윤 <jyun1937@naver.com>
2023-04-26 09:24:45 +0900

    img url 변경

--- js/main.js
+++ js/main.js
@@ -3,17 +3,11 @@ window.addEventListener("scroll", () => {
     document.querySelector("header").classList.add("active");
     document
       .querySelector(".logo a img")
-      .setAttribute(
-        "src",
-        "http://woorifuturelab.com/wp-content/uploads/2023/03/logo.png"
-      );
+      .setAttribute("src", "./../img/logoblue.png");
   } else {
     document.querySelector("header").classList.remove("active");
     document
       .querySelector(".logo a img")
-      .setAttribute(
-        "src",
-        "http://www.woorifis.com/resources/images/front/layout/logo.png"
-      );
+      .setAttribute("src", "./../img/logowhite.png");
   }
 });
